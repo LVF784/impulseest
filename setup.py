d613a97467c2ccd0354f3b4d9fa73f25b8ab66f2
LVF784 <vfluan@gmail.com>
2020-12-16 16:43:40 -0300

    Version 0.0.4 with 1e-3 lower bound, working well.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="impulseestest", # Replace with your own username
-    version="0.0.1",
+    version="0.0.4",
     author="Luan V. Fiorio",
     author_email="vfluan@gmail.com",
     description="Nonparametric impulse response estimation",
